3a2dab81bf4a38f2099d16a0cf04b50c518728d2
Gabriel Leoni <gabriel.leoni.um@gmail.com>
2025-04-14 20:13:54 -0300

    updated url mapping

--- src/main/java/com/galaxydata/starscan/controller/FilmsController.java
+++ src/main/java/com/galaxydata/starscan/controller/FilmsController.java
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -75,7 +76,7 @@ public class FilmsController {
         }
     }
 
-    @GetMapping(params = "title")
+    @PostMapping(params = "title")
     @Operation(summary = "Get Film by title", description = "Retrieve details of Star Wars Film by their title.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "Successfully retrieved the Film",
--- src/main/java/com/galaxydata/starscan/controller/StarshipsController.java
+++ src/main/java/com/galaxydata/starscan/controller/StarshipsController.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -76,7 +77,7 @@ public class StarshipsController {
         }
     }
 
-    @GetMapping(params = "name")
+    @PostMapping(params = "name")
     @Operation(summary = "Get Starship by name", description = "Retrieve details of Star Wars Starships by their name.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "Successfully retrieved the Starship",
