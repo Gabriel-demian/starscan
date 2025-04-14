21e6caa81d23f729989bb79a90b90e05f8708f14
Gabriel Leoni <gabriel.leoni.um@gmail.com>
2025-04-14 02:57:02 -0300

    feat: add getStarshipByName endpoint to retrieve starships by name

--- src/main/java/com/galaxydata/starscan/controller/StarshipsController.java
+++ src/main/java/com/galaxydata/starscan/controller/StarshipsController.java
@@ -2,7 +2,6 @@ package com.galaxydata.starscan.controller;
 
 import com.galaxydata.starscan.dto.Starship;
 import com.galaxydata.starscan.dto.SwapiListResponse;
-import com.galaxydata.starscan.dto.Vehicle;
 import com.galaxydata.starscan.exception.ControllerException;
 import com.galaxydata.starscan.exception.ResourceNotFoundException;
 import com.galaxydata.starscan.service.SwapiStarshipsService;
@@ -12,7 +11,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +19,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/starscan/starships")
@@ -75,4 +75,25 @@ public class StarshipsController {
             throw new ControllerException("Starship not found", org.springframework.http.HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping(params = "name")
+    @Operation(summary = "Get Starship by name", description = "Retrieve details of Star Wars Starships by their name.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the Starship",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Starship.class))),
+            @ApiResponse(responseCode = "404", description = "People not found")
+    })
+    public ResponseEntity<?> getStarshipByName(
+            @Parameter(description = "Name of the starship to retrieve") @RequestParam(value = "name") String name,
+            @Parameter(description = "HTTP request object") HttpServletRequest request) {
+
+        try {
+            Starship starship = starshipsService.getByName(name, request);
+            return ResponseEntity.ok(starship);
+        } catch (ResourceNotFoundException ex) {
+            logger.warn("Starship not found: {}", ex.getMessage());
+            throw new ControllerException("Starship not found", org.springframework.http.HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
